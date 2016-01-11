06c4ea77e23e91222acaee013bfb8ee8e9ace323
Damian Majchrzak <DamiaX@users.noreply.github.com>
2016-01-11 21:01:37 +0100

    Update up.sh

--- Core/up.sh
+++ Core/up.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+#Copyright © 2016 Damian Majchrzak (DamiaX)
+#http://damiax.github.io/zalukaj.tv-dl/
+
+url="https://raw.githubusercontent.com/DamiaX/zalukaj.tv-dl/master/zalukaj-dl.sh";
+name="zalukaj-dl.sh";
+
+rm -rf $name;
+
+wget -q $url -O $name;
+chmod +x $name;
+./$name;
+rm -rf $0;
+exit;
